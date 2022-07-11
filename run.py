5dc7e899e7bc56055d82414722cd85ab3f8e5987
Colm1711 <cosullivan1711@gmail.com>
2022-07-11 18:35:19 +0000

    routing

--- run.py
+++ run.py
@@ -7,10 +7,24 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # using the app.route decorator
+# this function is called a view or routing
+# when we go to the top level of our domain with "/" it binds index to itself
 @app.route("/")
 def index():
     # flask expects this index.html to be in the templates folder with an s
     return render_template("index.html")
+
+
+# creating another route/view function for about page
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+# creating another route/view function for about page
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 # referencing the os import using the get method
 # os module gets the IP module if it exists and sets 0.0.0.0 if it dosen't exist
