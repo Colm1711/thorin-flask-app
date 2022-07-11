ce2c5fcf529a674a1fae059c4b0c344dfc02b2ca
Colm1711 <cosullivan1711@gmail.com>
2022-07-11 18:19:01 +0000

    hello flask

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import os
+from flask import Flask
+
+# creating an instance of this and storing it in app
+# naming convention in flask is to call it app
+# first arg is the name of the module(__name__ which is built in var of Python)
+app = Flask(__name__)
+
+# using the app.route decorator
+@app.route("/")
+def index():
+    return "Hello World!"
+
+# referencing the os import using the get method
+# os module gets the IP module if it exists and sets 0.0.0.0 if it dosen't exist
+# doing the same with port, using the 5000 as this is the common one used by flask
+# setting debug to True to help with our debug, this is should be set for production code or submitting project
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
