d21666a4a1b579b8afb08c08121f2eeac28336b8
Colm1711 <cosullivan1711@gmail.com>
2022-07-17 16:15:33 +0000

    flash messages with forms

--- run.py
+++ run.py
@@ -1,11 +1,14 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 # creating an instance of this and storing it in app
 # naming convention in flask is to call it app
 # first arg is the name of the module(__name__ which is built in var of Python)
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 # using the app.route decorator
 # this function is called a view or routing
@@ -36,8 +39,11 @@ def about_member(member_name):
 
 
 # creating another route/view function for about page
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
@@ -46,9 +52,12 @@ def careers():
     return render_template("careers.html", page_title="Careers")
 
 # referencing the os import using the get method
-# os module gets the IP module if it exists and sets 0.0.0.0 if it dosen't exist
-# doing the same with port, using the 5000 as this is the common one used by flask
-# setting debug to True to help with our debug, this is should be set for production code or submitting project
+# os module gets the IP module if it exists and sets 0.0.0.0 if it dosen't 
+# exist
+# doing the same with port, using the 5000 as this is the common one used by 
+# flask
+# setting debug to True to help with our debug, this is should be set for 
+# production code or submitting project
 
 if __name__ == "__main__":
     app.run(
